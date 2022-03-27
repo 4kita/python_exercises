177beda28d9d2c944f97beaac82c9bc6da4a5246
4kita <fclambert@protonmail.com>
2022-03-27 14:10:32 -0500

    update 8_7_1

--- review_8_7_1.py
+++ review_8_7_1.py
@@ -1,1 +1,20 @@
-print('test')
+while True:
+
+    try:
+        someNum = int(input("Enter a number: "))
+        someString = input("Enter a string: ")
+        #print(someNum)
+        print(someString)
+        someString[someNum]
+        print(someString[someNum])
+        someString[someNum] = 'y'
+        print(someString[someNum])
+
+        break
+
+    except(ValueError):
+        print("You didn't enter a number!")
+    except(IndexError):
+        print("Your integer was too large for the string!")
+
+
