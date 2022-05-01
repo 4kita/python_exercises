e4e028db21684a115bb10e43ea017d0093aade32
4kita <fclambert@protonmail.com>
2022-05-01 10:27:03 -0500

    add lists.py

---
+++ lists.py
@@ -0,0 +1,9 @@
+food = ['rice', 'beans']
+food.append('broccoli')
+addlFood = ['bread', 'pizza']
+food.extend(addlFood)
+#print(food[slice(2)])
+#print(food[4])
+breakfast = ("eggs, fruit, orange juice").split(",")
+print(breakfast)
+print(len(breakfast))
