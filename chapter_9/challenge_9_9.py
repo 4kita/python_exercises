f463e9cd68183e3b613ccee946a16a8c67011a89
4kita <fclambert@protonmail.com>
2022-05-22 10:46:53 -0500

    updated cats

--- chapter_9/challenge_9_9.py
+++ chapter_9/challenge_9_9.py
@@ -1,2 +1,16 @@
 number_of_cats = 100
 cats = [False] * number_of_cats
+
+print(cats)
+
+round = 1
+cat_number = 0
+
+while round <= 100:
+    while cat_number < 100:
+       cats[cat_number] = not cats[cat_number]
+       cat_number = cat_number + round    
+    cat_number = 0
+    round = round + 1
+
+print(cats)
