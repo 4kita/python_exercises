c8a92fe179a89dadd476d47d83b376087aba785b
4kita <fclambert@protonmail.com>
2022-05-22 14:41:22 -0500

    added review excercise 10_1

---
+++ chapter_10/10_1.py
@@ -0,0 +1,13 @@
+class Dog:
+    def __init__(self, name, age, color):
+        self.name = name
+        self.age = age
+        self.coat_color = color
+
+
+philo = Dog("Philo", 5, "brown")
+print(f"{philo.name}'s coat is {philo.coat_color}.")
+
+
+
+
