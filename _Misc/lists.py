2de5af0bdd9ace2f53b0725c177ae60f30ef2dbc
4kita <fclambert@protonmail.com>
2022-06-26 13:17:07 -0500

    deleted lists and tuples files

--- _Misc/lists.py
+++
@@ -1,9 +0,0 @@
-food = ['rice', 'beans']
-food.append('broccoli')
-addlFood = ['bread', 'pizza']
-food.extend(addlFood)
-#print(food[slice(2)])
-#print(food[4])
-breakfast = ("eggs, fruit, orange juice").split(",")
-print(breakfast)
-print(len(breakfast))
